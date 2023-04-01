6b03949ae68f4c57fcb8f2046d48c5c0e1bb1154
kareeem380 <karimbouaddi380@gmail.com>
2023-04-01 14:20:16 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a line with puts function 
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome us a piece of art, \n");
+return (0);
+}
