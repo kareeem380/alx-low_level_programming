31ed29ad1d75e91fe3fa6ae5d9a24cd44552131d
kareeem380 <karimbouaddi380@gmail.com>
2023-04-01 13:53:39 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main -A prohgram that prints a line with puts function 
+ * return : 0 (Success)
+ */
 int main(void)
 {
     puts("\"Programming is like building a multilingual puzzle");
 
-    return 0;
+    return (0);
 }
 
